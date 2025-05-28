66ab5a238e7df9ff2d6235737f36de7454828480
John Novak <john@johnovak.net>
2025-05-28 10:48:19 +1000

    fluidsynth: Disable platform specific features
    We don't care about any of this. We're only using FluidSynth as data in
    + data out.

--- vcpkg-ports/fluidsynth/portfile.cmake
+++ vcpkg-ports/fluidsynth/portfile.cmake
@@ -27,35 +27,16 @@ vcpkg_check_features(
         pulseaudio  enable-pulseaudio
 )
 
-# enable platform-specific features, force the build to fail if the required libraries are not found,
-# and disable all other features to avoid system libraries to be picked up
-set(WINDOWS_OPTIONS enable-dsound enable-wasapi enable-waveout enable-winmidi HAVE_MMSYSTEM_H HAVE_DSOUND_H HAVE_OBJBASE_H)
-set(MACOS_OPTIONS enable-coreaudio enable-coremidi COREAUDIO_FOUND COREMIDI_FOUND)
-set(LINUX_OPTIONS enable-alsa ALSA_FOUND)
-set(ANDROID_OPTIONS enable-opensles OpenSLES_FOUND)
-set(IGNORED_OPTIONS enable-coverage enable-dbus enable-floats enable-fpe-check enable-framework enable-jack
+set(OPTIONS_TO_DISABLE enable-coverage enable-dbus enable-floats enable-fpe-check enable-framework enable-jack
     enable-libinstpatch enable-midishare enable-oboe enable-openmp enable-oss enable-pipewire enable-portaudio
-    enable-profiling enable-readline enable-sdl2 enable-sdl3 enable-systemd enable-trap-on-fpe enable-ubsan)
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    set(OPTIONS_TO_ENABLE ${WINDOWS_OPTIONS})
-    set(OPTIONS_TO_DISABLE ${MACOS_OPTIONS} ${LINUX_OPTIONS} ${ANDROID_OPTIONS})
-elseif(VCPKG_TARGET_IS_OSX)
-    set(OPTIONS_TO_ENABLE ${MACOS_OPTIONS})
-    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${LINUX_OPTIONS} ${ANDROID_OPTIONS})
-elseif(VCPKG_TARGET_IS_LINUX)
-    set(OPTIONS_TO_ENABLE ${LINUX_OPTIONS})
-    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${MACOS_OPTIONS} ${ANDROID_OPTIONS})
-elseif(VCPKG_TARGET_IS_ANDROID)
-    set(OPTIONS_TO_ENABLE ${ANDROID_OPTIONS})
-    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${MACOS_OPTIONS} ${LINUX_OPTIONS})
-endif()
+    enable-profiling enable-readline enable-sdl2 enable-sdl3 enable-systemd enable-trap-on-fpe enable-ubsan
+    enable-dsound enable-wasapi enable-waveout enable-winmidi enable-coreaudio enable-coremidi enable-alsa enable-opensles)
 
 foreach(_option IN LISTS OPTIONS_TO_ENABLE)
     list(APPEND ENABLED_OPTIONS "-D${_option}:BOOL=ON")
 endforeach()
-    
-foreach(_option IN LISTS OPTIONS_TO_DISABLE IGNORED_OPTIONS)
+
+foreach(_option IN LISTS OPTIONS_TO_DISABLE)
     list(APPEND DISABLED_OPTIONS "-D${_option}:BOOL=OFF")
 endforeach()
 
@@ -93,6 +74,6 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/share/man")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-    
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
